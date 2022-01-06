729238b9a49a4e500dc60753b7279bb91205e75e
Victoria Fahey <victoria.fahey10@gmail.com>
2022-01-06 16:37:30 +1300

    store data on smart contract

--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,31 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners()
     const waveContractFactory = await hre.ethers.getContractFactory('WavePortal')
     const waveContract = await waveContractFactory.deploy()
     await waveContract.deployed()
+
     console.log("Contract deployed to:", waveContract.address)
+    console.log("Contract deployed by:", owner.address)
+
+    let waveCount
+    waveCount = await waveContract.getTotalWaves()
+
+    let waveTxn = await waveContract.wave()
+    await waveTxn.wait()
+
+    waveCount = await waveContract.getTotalWaves()
+
+    waveTxn = await waveContract.connect(randomPerson).wave()
+
+    waveCount = await waveContract.getTotalWaves()
+
+    // storing list of addresses in an array
+    const senderAddressStore = []
+    senderAddressStore.push(waveTxn)
+    const senderAddressList = senderAddressStore.map(sender => {
+        return sender.from
+    })
+    console.log(senderAddressList)
 }
 
 const runMain = async () => {
