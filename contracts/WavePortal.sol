a907307a88560b451356976f65b311294d024f53
Victoria Fahey <victoria.fahey10@gmail.com>
2022-01-14 12:11:31 +1300

    add 15min cooldown between waves to contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -23,6 +23,11 @@ contract WavePortal {
     // can hold all the waves anyone ever sends
     Wave[] waves;
 
+    // mapping = key value pairs
+    // mapping an address with a number
+    // here we are storing the address with the last time the user waved at us
+    mapping(address => uint256) public lastWavedAt;
+
     constructor() payable {
         console.log("I am a contract and I am smart");
         // set the initial seed:
@@ -33,6 +38,15 @@ contract WavePortal {
     // function requires a string called _message
     // message the user sends us from the frontend
     function wave(string memory _message) public {
+        // make sure the current timestamp is at least 15-minutes bigger than the last timestamp we stored
+        require(
+            lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
+            "wait 15m"
+        );
+
+        // update the current timestamp we have for the user
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalWaves += 1;
         console.log("%s has waved! with message: %s", msg.sender, _message);
 
